734397eb4c628f0db8254c0b0c8cae9d86db1ae6
rockerBOO <rockerboo@gmail.com>
2020-07-17 23:44:56 -0400

    Fixing tests for new tags

--- test/irc/login_test.exs
+++ test/irc/login_test.exs
@@ -2,12 +2,6 @@ defmodule Blur.IRC.LoginTest do
   use ExUnit.Case, async: true
   doctest Blur.IRC.Login
 
-  test "on logged in" do
-    irc_client = start_supervised!({ExIRC.Client, []})
-
-    {:noreply, _} = Blur.IRC.Login.handle_info(:logged_in, %{client: irc_client, autojoin: []})
-  end
-
   test "to autojoin channels in the config" do
     irc_client = start_supervised!({ExIRC.Client, []})
 
